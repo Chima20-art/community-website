1faa3e3935508c75d6a0d17ea5f50c718a5691a9
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2021-08-10 15:36:47 +0300

    porduct page finished

--- src/products/products.js
+++ src/products/products.js
@@ -6,14 +6,38 @@ export default function Product() {
     <div>
       <div className={styles.redDiv}>
         <div className={styles.title}>
-          The most advanced technology always work hand-in-hand with qualified
-          people
+          The most advanced technology always
+          <b> work hand-in-hand with qualified people</b>
         </div>
       </div>
       <div className={styles.grayDiv}></div>
 
       <div className={styles.productParagraph}>
-        Our product, hdfgwvflujhwjvnefbvhkef bfdhvkef kjbvfk;s
+        <div className={styles.text}>
+          <div className={styles.ourProductTitle}>
+            <div> Our Product</div>
+            <img src="/Line 1-2.png" />
+          </div>
+
+          <div className={styles.paragraph}>
+            <p>
+              Our product is made on the base of our team’s careful research and
+              analyses, ranging from internet based application such as Internet
+              Banking; mobile frontend application such as Mobile Banking;
+              transaction based banking such as EDC Banking; and other financial
+              platforms such as Personal Investment Portfolio.
+            </p>
+            <p>
+              Our product and service range will technically safeguard your
+              business operations through continued application development. It
+              is all topped with our integrated procedure made in accordance
+              with the industry’s best practices, which will drive the built-in
+              system towards the creation of secured and value added services
+              for you as business partners.
+            </p>
+          </div>
+        </div>
+        <img src="/computer.png" className={styles.computer} />
       </div>
     </div>
   );
