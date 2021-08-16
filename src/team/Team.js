90c12b5a9be577d08851e08ea39657ca19f4d2e4
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2021-08-16 17:48:54 +0300

    one carditem designed.

--- src/team/Team.js
+++ src/team/Team.js
@@ -12,7 +12,27 @@ export default function Team() {
         <div className={styles.image}>
           <img src="/cuate.png" />
         </div>
-        <div className={styles.redBorder}></div>
+        <div className={styles.redBorder} />
+      </div>
+      <div className={styles.management}>
+        <div className={styles.manajmenTitle}>
+          Manajemen
+          <img src="/Line 1.png" className={styles.vector} />
+        </div>
+        <div className={styles.cardItems}>
+          <div className={styles.cardItem}>
+            <div>
+              <img src="/circle-box.png" className={styles.picture} />
+            </div>
+            <div className={styles.cardTitle}>President Director</div>
+            <div className={styles.name}>Rodney Stratton</div>
+            <div className={styles.media}>
+              <img className={styles.facebook} src="/Facebook.png" />
+              <img className={styles.tweeter} src="/Twitter.png" />
+              <img className={styles.instagram} src="/Instagram.png" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
