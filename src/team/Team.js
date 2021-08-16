35d97306578bf1719e53bba30b008b43ffa02d80
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2021-08-16 18:01:29 +0300

    managementDiv finished.

--- src/team/Team.js
+++ src/team/Team.js
@@ -20,11 +20,23 @@ export default function Team() {
           <img src="/Line 1.png" className={styles.vector} />
         </div>
         <div className={styles.cardItems}>
-          <div className={styles.cardItem}>
+          <div className={styles.cardItem} style={{ marginRight: "100px" }}>
             <div>
               <img src="/circle-box.png" className={styles.picture} />
             </div>
             <div className={styles.cardTitle}>President Director</div>
+            <div className={styles.name}>Rodney Stratton</div>
+            <div className={styles.media}>
+              <img className={styles.facebook} src="/Facebook.png" />
+              <img className={styles.tweeter} src="/Twitter.png" />
+              <img className={styles.instagram} src="/Instagram.png" />
+            </div>
+          </div>
+          <div className={styles.cardItem} style={{ marginLeft: "100px" }}>
+            <div>
+              <img src="/circle-box-2.png" className={styles.picture} />
+            </div>
+            <div className={styles.cardTitle}> Director</div>
             <div className={styles.name}>Rodney Stratton</div>
             <div className={styles.media}>
               <img className={styles.facebook} src="/Facebook.png" />
