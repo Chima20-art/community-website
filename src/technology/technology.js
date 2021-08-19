32ce928698bf02fbba6e5f9e1dd0bbe2d9dcc540
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2021-08-19 21:08:20 +0300

    our technology div finished.

--- src/technology/technology.js
+++ src/technology/technology.js
@@ -24,6 +24,41 @@ export default function technology() {
         <img src="/html_5.png" className={styles.logo} />
         <img src="/microsoft.png" className={styles.logo} />
       </div>
+      <div className={styles.ourTechnologyDiv}>
+        <div className={styles.Title}>
+          Client Line-Up
+          <img src="/Line 1.png" className={styles.vector} />
+          <div className={styles.technologyText}>
+            <div className={styles.paragraph}>
+              First JAVA, runs on more than 850 million personal computers
+              worldwide, and on billions of devices worldwide, including mobile
+              and TV devices.The latest Java version contains important
+              enhancements to improve performance, stability and security of the
+              Java applications that run on your machine. Installing this free
+              update will ensure that your Java applications continue to run
+              safely and efficiently. The best part of JAVA is free.
+            </div>
+            <div className={styles.paragraph}>
+              We also use PHP for general occation, why? we think you already
+              know the reason,of course because is the most flexible language on
+              earth that can colaborate very well with all language.
+            </div>
+            <div className={styles.paragraph}>
+              {" "}
+              for the strusture is HTML & CSS ( who does not know ? ) cause it's
+              the basic language use for the web and help us creating a very
+              outstanding view. its become important cause it make our system
+              looks good in front of the eyes of our client, (doesnt agree?),
+              Hmm, let me ask you if you have a good software but with bad
+              UI/UX, do people use it? cause for it we can reach our goals.
+            </div>
+            <div className={styles.paragraph}>
+              What about Microsoft technology, why not. . . We always strive to
+              remain the best and try to keep developing what we have.
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
