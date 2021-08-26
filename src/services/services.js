18a1dfc29d1e14c1dcfc11f7c15fa7126cc4cb61
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2021-08-26 12:08:44 +0300

    added some modifications to ourServices div.

--- src/services/services.js
+++ src/services/services.js
@@ -21,30 +21,32 @@ export default function services() {
             <div className={styles.text}>Our services</div>
             <img src="/Line 1.png" className={styles.titleLine} />
           </div>
-          <p className={styles.paragraph}>
-            DSI’s shared service solutions focus on the front-end based software
-            development, designed specifically for the banking and financial
-            sectors. We short-cut your front-liners to customers interaction
-            process through a way that mediates your business process in a more
-            direct communication means, in just a smile away from winning your
-            customer’s trust.
-          </p>
-          <p className={styles.paragraph}>
-            Through close partnership, we always work together with your team,
-            all throughout from pretopost project cycles – initial planning,
-            project development, implementation, evaluation up to reporting
-            documentation – to fully coach you so that all your specific
-            business.
-          </p>
-          <p className={styles.paragraph}>
-            requirements can be delivered at the right time to the right
-            audience of particular interests. Moreover, our team will support
-            you to provide a detailed consultation throughout operation stage,
-            and after the project implementation cycles by applying specific
-            monitoring and improvement methodology. Our experienced people will
-            drive your personalized service, by utilizing the best technology
-            supports no other businesses can provide.
-          </p>
+          <div>
+            <p className={styles.paragraph}>
+              DSI’s shared service solutions focus on the front-end based
+              software development, designed specifically for the banking and
+              financial sectors. We short-cut your front-liners to customers
+              interaction process through a way that mediates your business
+              process in a more direct communication means, in just a smile away
+              from winning your customer’s trust.
+            </p>
+            <p className={styles.paragraph}>
+              Through close partnership, we always work together with your team,
+              all throughout from pretopost project cycles – initial planning,
+              project development, implementation, evaluation up to reporting
+              documentation – to fully coach you so that all your specific
+              business.
+            </p>
+            <p className={styles.paragraph}>
+              requirements can be delivered at the right time to the right
+              audience of particular interests. Moreover, our team will support
+              you to provide a detailed consultation throughout operation stage,
+              and after the project implementation cycles by applying specific
+              monitoring and improvement methodology. Our experienced people
+              will drive your personalized service, by utilizing the best
+              technology supports no other businesses can provide.
+            </p>
+          </div>
         </div>
 
         <div className={styles.chatImage}>
