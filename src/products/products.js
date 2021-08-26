25a031bbd87e0a655d382f3a467fcd680cdefe3e
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2021-08-26 14:14:38 +0300

    finished media queries for second div of products page.

--- src/products/products.js
+++ src/products/products.js
@@ -3,7 +3,7 @@ import styles from "./products.module.css";
 
 export default function Product() {
   return (
-    <div>
+    <div className={styles.container}>
       <div className={styles.redDiv}>
         <div className={styles.title}>
           The most advanced technology always
@@ -11,33 +11,35 @@ export default function Product() {
         </div>
       </div>
       <div className={styles.grayDiv}></div>
-
       <div className={styles.productParagraph}>
-        <div className={styles.text}>
-          <div className={styles.ourProductTitle}>
-            <div> Our Product</div>
-            <img src="/Line 1-2.png" />
-          </div>
+        <div className={styles.flex}>
+          <div className={styles.text}>
+            <div className={styles.ourProductTitle}>
+              <div> Our Product</div>
+              <img src="/Line 1-2.png" className={styles.lineImage} />
+            </div>
 
-          <div className={styles.paragraph}>
-            <p>
-              Our product is made on the base of our team’s careful research and
-              analyses, ranging from internet based application such as Internet
-              Banking; mobile frontend application such as Mobile Banking;
-              transaction based banking such as EDC Banking; and other financial
-              platforms such as Personal Investment Portfolio.
-            </p>
-            <p>
-              Our product and service range will technically safeguard your
-              business operations through continued application development. It
-              is all topped with our integrated procedure made in accordance
-              with the industry’s best practices, which will drive the built-in
-              system towards the creation of secured and value added services
-              for you as business partners.
-            </p>
+            <div className={styles.paragraph}>
+              <p className={styles.paragraph1}>
+                Our product is made on the base of our team’s careful research
+                and analyses, ranging from internet based application such as
+                Internet Banking; mobile frontend application such as Mobile
+                Banking; transaction based banking such as EDC Banking; and
+                other financial platforms such as Personal Investment Portfolio.
+              </p>
+              <p>
+                Our product and service range will technically safeguard your
+                business operations through continued application development.
+                It is all topped with our integrated procedure made in
+                accordance with the industry’s best practices, which will drive
+                the built-in system towards the creation of secured and value
+                added services for you as business partners.
+              </p>
+            </div>
           </div>
+          <img src="/computer.png" className={styles.computer} />
         </div>
-        <img src="/computer.png" className={styles.computer} />
+        <img src="/Line 1-2.png" className={styles.lineImage} />
       </div>
     </div>
   );
