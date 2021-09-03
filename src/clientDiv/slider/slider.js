16df3fc8b215cfc9b33a44c0f39d43e4d5b2290e
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2021-09-04 00:15:24 +0300

    testimonial div adjusted for mobile .

--- src/clientDiv/slider/slider.js
+++ src/clientDiv/slider/slider.js
@@ -6,7 +6,7 @@ import styles from "./slider.module.css";
 const SimpleSlider = () => (
   <Carousel plugins={["arrows"]}>
     <div className={styles.section}>
-      <img src="/Mark.png" />
+      <img src="/Mark.png" className={styles.sectionImage} />
       <div className={styles.description}>
         <img src="/quote icon.png" className={styles.quote} />
         <p className={styles.paragraph}>
